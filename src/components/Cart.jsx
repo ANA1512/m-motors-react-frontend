181194c231d942d1dffd46fc71857001f77b5fa7
Philocles anais <anais_philocles@yahoo.fr>
2026-06-10 22:05:56 +0200

    create files from cart page - link bd

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,35 @@
 
 import{useState} from 'react'
 
+
 function Cart() {
 
     const [cart, setCart] = useState(JSON.parse(localStorage.getItem("data")) || [])
     console.log("cart:", cart)
+    const[revenuMensuel, setRevenuMensuel] = useState('')
+    
+  const handleCreateFiles = (e, vehicule) => {
+    e.preventDefault()
+
+    const sendDataFiles = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            vehicule_id: vehicule.id,
+            type_financement: vehicule.type,
+            revenu_mensuel: parseInt(revenuMensuel),
+            statut: 'en attente'
+        })
+    }
+
+    fetch('http://127.0.0.1:5001/dossier', sendDataFiles)
+        .then(response => response.json())
+        .then(data => alert('Dossier envoyé et en cours d\'examen'))
+}
+  
 
   return (
     <div>
@@ -13,6 +38,19 @@ function Cart() {
             {vehicule.name} -
             {vehicule.type} -
             {vehicule.price} -
+            
+            <form>
+           <input 
+                type="text" 
+                value={revenuMensuel}
+                onChange={(e) => setRevenuMensuel(e.target.value)} 
+           />
+          <input 
+            onClick={(e) => handleCreateFiles(e, vehicule)}
+            type="submit"
+            value="Créer un dossier"
+          />
+      </form>
         </li>
 ))}
     </div>
